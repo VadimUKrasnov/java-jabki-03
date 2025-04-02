d563a92eaaf2dff5b88c9bfdd02dc6e1c76ac27a
Vadim Krasnov <wadsonred@gmail.com>
2025-04-02 21:17:01 +0300

    java_jabki_3

--- Main.java
+++ Main.java
@@ -89,5 +89,77 @@ public class Main {
             }
             System.out.println(i);
         }
+
+        System.out.println();
+
+        /**
+         * ДЗ-3
+         */
+        // if-else
+        // 1. Дано число. Напишите программу, которая определяет, является ли оно четным или нечетным
+        int anyNumber = 8;
+        if (anyNumber % 2 == 0) {
+            System.out.println("Число чётное");
+        } else {
+            System.out.println("Число не чётное");
+        }
+
+        System.out.println();
+
+        // 2. Даны два числа. Выведите «Первое больше», «Второе больше» или «Равны»
+        int num1 = 5;
+        int num2 = 5;
+        if (num1 > num2) {
+            System.out.println("Первое больше");
+        } else if (num1 < num2) {
+            System.out.println("Второе больше");
+        } else{
+            System.out.println("Равны");
+        }
+
+        System.out.println();
+
+        // 3. Дано число. Выведите «Положительное», «Отрицательное» или «Равно нулю»
+        int number = 0;
+        if (number > 0) {
+            System.out.println("Положительное");
+        } else if (number < 0) {
+            System.out.println("Отрицательное");
+        } else if (number == 0) {
+            System.out.println("Равно нулю");
+        }
+
+        System.out.println();
+
+        // 4. Дано число (возраст). Если возраст >= 18, выведите «Доступ разрешен», иначе — «Доступ запрещен
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Доступ разрешен");
+        } else {
+            System.out.println("Доступ запрещён");
+        }
+
+        System.out.println();
+
+        // 5. Придумать задачу самим
+        double bodyTemperature = 36.6;
+        if (bodyTemperature != 36.6) {
+            System.out.println("Обратитесь к врачу!");
+        } else {
+            System.out.println("Здоров!");
+        }
+
+        System.out.println();
+
+        // Тернарный оператор
+        // 1. Дано число. Используя тернарный оператор, определите, положительное, отрицательное или нулевое
+        int numb = -1;
+        System.out.println(numb > 0 ? "Положительное" : numb < 0 ? "Отрицательное" : "Нулевое");
+
+        System.out.println();
+
+        // 2. Дано число. С помощью тернарного оператора проверьте, делится ли оно на 3 и 5 одновременно. Выведите «Кратно» или «Не кратно»
+
+
     }
 }
